f370b15908f0803ca45d40651c05dcc421aacc95
Mardin <mardineliassi57@gmail.com>
2022-01-21 14:11:25 +0100

    Small changes.

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] intArray = new int[5];
-
-
-        linearSearch(intArray,44);
+        int[] intArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        linearSearch(intArray, 5);
     }
 
     public static int linearSearch(int[] array, int target){
         // LINEAR SEARCH - SEQUENTIAL SEARCH
-        array = new int[]{1,2,3,4,5,6,7,8,9,10,11};
         for(int i = 0; i<array.length; i++){
             if(array[i] == target){
                 System.out.println("Target found in the array: "+target);
                 return target;
-
             }
         }
-        System.out.println("Not in the array");
+        System.out.println("Not found in the array - worst case scenario");
         return -1;
     }
 }
